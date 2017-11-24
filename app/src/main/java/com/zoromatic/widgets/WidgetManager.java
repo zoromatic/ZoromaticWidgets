79889a255e8116164eeaa8b558d0f7db19cde6ce
zoromatic <zoromatic@gmail.com>
2017-11-24 17:31:50 +0100

    Refactoring

---
+++ app/src/main/java/com/zoromatic/widgets/WidgetManager.java
@@ -0,0 +1,22 @@
+package com.zoromatic.widgets;
+
+import android.content.Context;
+
+/**
+ * Created by zoran on 11/24/2017.
+ * Contains methods for updating, setting and getting info
+ * about AppWidgets
+ */
+
+public class WidgetManager {
+    Context mContext;
+
+    /**
+     * Creates a new instance of <code>WidgetManager</code> with the given parameters.
+     *
+     * @param context  The context of caller object
+     */
+    public WidgetManager(Context context) {
+        mContext = context;
+    }
+}
